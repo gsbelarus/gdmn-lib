e1a9f62dbe1ee86633f0947e6ae7055a7183f360
andreik <gs1994@gmail.com>
2025-01-03 17:40:15 +0100

    feat: add base64 conversion functions and update package version to 0.1.2

---
+++ .changeset/calm-icons-own.md
@@ -0,0 +1,5 @@
+---
+"@gsbelarus/utils": minor
+---
+
+added functions for converting between base64 and uint8 array
--- packages/gsbelarus-utils/src/base64.ts
+++ packages/gsbelarus-utils/src/base64.ts
@@ -1,21 +1,49 @@
-const convertStringToBytes = (text: string) => {
-  const textEncoder = new TextEncoder();
-  const uint8Array = textEncoder.encode(text);
-  const bytes = Array.from(uint8Array);
-  return bytes;
-};
+export function uint8ArrayToBase64(uint8Array: Uint8Array): string {
+  if (typeof btoa !== 'undefined') {
+    let binaryString = '';
+    uint8Array.forEach(byte => {
+      binaryString += String.fromCharCode(byte);
+    });
+    return btoa(binaryString);
+  } else if (typeof Buffer !== 'undefined') {
+    return Buffer.from(uint8Array).toString('base64');
+  } else {
+    throw new Error('Environment does not support Base64 encoding');
+  }
+}
 
-const convertBytesToString = (bytes: number[]) => {
-  const textDecoder = new TextDecoder();
-  const uint8Array = new Uint8Array(bytes);
-  const text = textDecoder.decode(uint8Array);
-  return text;
-};
+export function base64ToUint8Array(base64String: string): Uint8Array {
+  if (typeof Buffer !== 'undefined') {
+    const buffer = Buffer.from(base64String, 'base64');
+    return new Uint8Array(buffer);
+  } else if (typeof atob !== 'undefined') {
+    const binaryString = atob(base64String);
+    const length = binaryString.length;
+    const uint8Array = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+      uint8Array[i] = binaryString.charCodeAt(i);
+    }
+    return uint8Array;
+  } else {
+    throw new Error('Environment does not support Base64 decoding');
+  }
+}
 
-export const encodeToBase64 = (input: string) => {
-  return btoa(convertStringToBytes(input).join(','));
-};
+export function uint8ArrayToUtf8String(uint8Array: Uint8Array): string {
+  const decoder = new TextDecoder('utf-8');
+  return decoder.decode(uint8Array);
+}
 
-export const decodeFromBase64 = (base64: string) => {
-  return convertBytesToString(atob(base64).split(',').map(Number));
-};
+export function numberToUint8Array32(number: number): Uint8Array {
+  const buffer = new ArrayBuffer(4);
+  const view = new DataView(buffer);
+  view.setFloat32(0, number, true);
+  return new Uint8Array(buffer);
+}
+
+export function uint8ArrayToNumber32(uint8Array: Uint8Array): number {
+  const buffer = uint8Array.buffer;
+  const view = new DataView(buffer);
+  return view.getFloat32(0, true);
+}
+
