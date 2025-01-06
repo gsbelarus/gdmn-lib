b5f5a8a017303fe53b826de1e5c02add6ad14615
andreik <gs1994@gmail.com>
2025-01-06 20:42:38 +0100

    feat: bump gdmn-utils version to 0.6.0; add EMAIL_REGEXP constant and update changelog

--- packages/gdmn-utils/CHANGELOG.md
+++ packages/gdmn-utils/CHANGELOG.md
@@ -1,5 +1,11 @@
 # gdmn-utils
 
+## 0.6.0
+
+### Minor Changes
+
+- some useful constants added
+
 ## 0.5.0
 
 ### Minor Changes
--- packages/gdmn-utils/src/index.ts
+++ packages/gdmn-utils/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./base64";
 export * from "./utils";
 export * from "./semaphore";
-
+export * from "./httpStatusCodes"; 
