c3e8133f2ef6828937227c4f10882ac95b8787a6
andreik <gs1994@gmail.com>
2025-01-10 21:16:01 +0100

    feat: update gdmn-er-ui to version 0.2.0; add toolbar component with customizable items

--- packages/gdmn-er-ui/CHANGELOG.md
+++ packages/gdmn-er-ui/CHANGELOG.md
@@ -1,5 +1,9 @@
 # gdmn-er-ui
 
+## 0.2.0
+
+### Minor Changes
+
+- toolbar items added
+
 ## 0.1.0
-
-
--- packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
+++ packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
@@ -1,5 +1,40 @@
-export function GdmnToolbar() {
+import React from 'react';
+
+export type GdmnToolbarItem = {
+  type: 'button';
+  id: string;
+  title: string;
+  icon: string | React.ReactNode;
+  onClick: () => void;
+} | {
+  type: 'separator';
+};
+
+export type GdmnToolbarItems = GdmnToolbarItem[];
+
+type GdmnToolbarProps = {
+  items: GdmnToolbarItems;
+};
+
+export function GdmnToolbar({ items }: GdmnToolbarProps) {
   return (
-    <div>Toolbar...</div>
+    <div className='flex flex-row items-center gap-2 p-2'>
+      {
+        items.map((item, index) => {
+          if (item.type === 'button') {
+            return (
+              <button key={index} onClick={item.onClick}>
+                {item.icon}
+                {item.title}
+              </button>
+            );
+          } else if (item.type === 'separator') {
+            return (
+              <div key={index} style={{ borderLeft: '1px solid black', height: '100%' }} />
+            );
+          }
+        })
+      }
+    </div>
   );
-}
+};
