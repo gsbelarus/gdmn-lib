e362e970071fc4e9611c3ef633421299a8b89fe4
andreik <gs1994@gmail.com>
2025-01-26 18:41:34 +0100

    feat: bump version to 0.4.0; enhance GdmnToolbar with optional labels and improved button handling

--- packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
+++ packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
@@ -11,44 +11,52 @@ export type GdmnToolbarItem = {
   disabled?: boolean;
   attention?: boolean;
   loading?: boolean;
-  onClick: () => void;
+  onClick?: () => void;
 } | {
   type: 'separator';
 };
 
-export type GdmnToolbarItems = GdmnToolbarItem[];
+type GdmnToolbarItems = GdmnToolbarItem[];
 
 type GdmnToolbarProps = {
   items: GdmnToolbarItems;
+  showLabels?: boolean;
 };
 
-export function GdmnToolbar({ items }: GdmnToolbarProps) {
+export function GdmnToolbar({ items, showLabels }: GdmnToolbarProps) {
   return (
     <div className='flex flex-row items-center gap-2 p-2'>
       {
         items.map((item, index) => {
-          if (item.type === 'button') {
-            return (
-              item.tooltip
-                ?
-                <Tooltip key={index} title={item.tooltip}>
-                  <button key={index} onClick={item.onClick}>
-                    {item.icon}
-                    {item.label}
-                  </button>
-                </Tooltip>
-                :
-                <button key={index} onClick={item.onClick}>
-                  {item.icon}
-                  {item.label}
-                </button>
-            );
-          } else if (item.type === 'separator') {
-            return (
-              <div key={index} style={{ borderLeft: '1px solid black', height: '100%' }} />
-            );
-          }
-        })
+          const B = item.type === 'button'
+            ?
+            <button
+              key={index}
+              onClick={item.disabled ? undefined : item.onClick}
+            >
+              <div className={item.loading ? 'animate-spin' : ''}>
+                {item.icon}
+              </div>
+              {showLabels && item.label}
+            </button>
+            :
+            undefined;
+
+          return (
+            B && item.type === 'button'
+              ?
+              <div
+                key={index}
+                className={`w-8 h-8 p-1 flex justify-center items-center border border-solid border-zinc-600 rounded fill-zinc-600 text-zinc-600
+                        hover:bg-primaryHover/20 hover:fill-primary hover:text-primary ${item.disabled ? 'opacity-50' : ''}`}
+              >
+                {item.tooltip ? <Tooltip key={index} title={item.tooltip}>{B}</Tooltip> : B}
+              </div>
+              :
+              <div key={index} className='h-full border-0 border-l border-solid border-zinc-500' />
+          );
+        }
+        )
       }
     </div>
   );
