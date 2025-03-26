62d0ec62c8714e50810ac0c957b216822b6b2c07
sashazernin <94962537+sashazernin@users.noreply.github.com>
2025-03-26 15:22:42 +0300

    disabled and first button fix

--- packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
+++ packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
@@ -37,14 +37,14 @@ export function GdmnToolbar({ items, showLabels }: GdmnToolbarProps) {
   const [internalLoading, setInternalLoading] = useState<string | number | undefined>();
 
   useEffect(() => {
-    if (pressed) {
+    if (pressed || pressed === 0) {
       const timeout = setTimeout(() => setPressed(undefined), 400);
       return () => clearTimeout(timeout);
     }
   }, [pressed]);
 
   useEffect(() => {
-    if (animated) {
+    if (animated || animated === 0) {
       const timeout = setTimeout(() => setAnimated(undefined), 400);
       return () => clearTimeout(timeout);
     }
@@ -80,7 +80,7 @@ export function GdmnToolbar({ items, showLabels }: GdmnToolbarProps) {
                 ? "bg-zinc-700 fill-zinc-50 text-zinc-50"
                 : "bg-primary fill-white text-white hover:bg-primary/80 hover:fill-white hover:text-white"
               : item.disabled
-                ? "fill-zinc-600 text-zinc-600 stroke-zinc-600 bg-opacity-40 grayscale"
+                ? "fill-zinc-600 text-zinc-600 opacity-40"
                 : "fill-zinc-600 text-zinc-600 hover:bg-primaryHover/20 hover:fill-primary hover:text-primary";
 
         if (index === pressed) {
@@ -92,7 +92,7 @@ export function GdmnToolbar({ items, showLabels }: GdmnToolbarProps) {
         return B && item.type === "button" ? (
           <div
             key={index}
-            className={`w-8 h-8 p-1 flex justify-center items-center border border-solid border-zinc-600 rounded ${st}`}
+            className={`w-8 h-8 flex justify-center items-center border border-solid border-zinc-600 rounded ${st}`}
             onClick={
               item.disabled || item.loading || !item.onClick
                 ? undefined
