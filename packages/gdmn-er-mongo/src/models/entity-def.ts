b534a8fa4918f2b1dc10e0635357e4e3baca3084
hirurg-lybitel <yurashoihet@gmail.com>
2025-04-10 21:00:33 +0300

    testing

--- packages/gdmn-er-mongo/src/models/entity-def.ts
+++ packages/gdmn-er-mongo/src/models/entity-def.ts
@@ -1,16 +1,16 @@
 import { entityDef } from 'gdmn-er';
 import { TEntityDefWithId, ZodEntityDef } from '../types/entity-def';
-import mongoose, { Model, Schema } from 'mongoose';
+import mongoose, { Model, Schema, Types } from 'mongoose';
 import { registerModel } from '../registry';
 import { createSchema } from "@mirite/zod-to-mongoose";
 import { entity2schema } from '../er2mongo';
 import z from 'zod';
 
-// const entityDefSchema = entity2schema<TEntityDefWithId>(entityDef);
+const entityDefSchema = entity2schema<TEntityDefWithId>(entityDef);
 
-const entityDefSchema = convertZodToMongoose(ZodEntityDef);
+const entityDefSchema_test = convertZodToMongoose(ZodEntityDef);
 
-console.log('entityDefSchema', entityDefSchema);
+console.log('entityDefSchema', { schema_1: entityDefSchema.obj, schema_2: entityDefSchema_test.obj });
 
 export const EntityDef = registerModel<TEntityDefWithId>(
   mongoose.models[entityDef.name] || mongoose.model(entityDef.name, entityDefSchema)
@@ -25,41 +25,59 @@ function convertZodToMongoose(zodSchema: any): Schema {
 
   // Функция для обработки типа Zod-схемы
   function processZodField(zodField: any): any {
+    let isOptional = false;
+    let isNullable = false;
+  
+    while (
+      zodField instanceof z.ZodOptional ||
+      zodField instanceof z.ZodNullable
+    ) {
+      if (zodField instanceof z.ZodOptional) isOptional = true;
+      if (zodField instanceof z.ZodNullable) isNullable = true;
+      zodField = zodField.unwrap();
+    }
+  
+    let fieldDef: any = {};
+  
     if (zodField instanceof z.ZodString) {
-      return { type: String };
+      fieldDef.type = String;
+    } else if (zodField instanceof z.ZodNumber) {
+      fieldDef.type = Number;
+    } else if (zodField instanceof z.ZodBoolean) {
+      fieldDef.type = Boolean;
+    } else if (zodField instanceof z.ZodArray) {
+      fieldDef.type = [processZodField(zodField.element)];
+    } else if (zodField instanceof z.ZodEnum) {
+      fieldDef.type = String;
+      fieldDef.enum = zodField._def.values;
+    } else if (zodField instanceof z.ZodObject) {
+      fieldDef = new Schema(processZodSchema(zodField), { _id: false });
+    } else if (zodField instanceof z.ZodRecord) {
+      fieldDef = {
+        type: Map,
+        of: processZodField(zodField._def.valueType),
+      };
+    } else if (
+      zodField._def?.typeName === "ZodInstance" &&
+      zodField._def?.ctor === Types.ObjectId
+    ) {
+      fieldDef.type = Types.ObjectId;
+    } else {
+      fieldDef.type = Schema.Types.Mixed;
     }
-    if (zodField instanceof z.ZodNumber) {
-      return { type: Number };
+  
+    if (!isOptional) {
+      fieldDef.required = true;
     }
-    if (zodField instanceof z.ZodBoolean) {
-      return { type: Boolean };
-    }
-    if (zodField instanceof z.ZodArray) {
-      return { type: [processZodField(zodField.element)] };
-    }
-    if (zodField instanceof z.ZodEnum) {
-      return { type: String, enum: zodField._def.values };
-    }
-    if (zodField instanceof z.ZodObject) {
-      return new Schema(processZodSchema(zodField));
-    }
-    if (zodField instanceof z.ZodOptional) {
-      return { type: processZodField(zodField.unwrap()), required: false };
-    }
-    if (zodField instanceof z.ZodNullable) {
-      return { type: processZodField(zodField.unwrap()), required: false };
-    }
-    // Обработка других типов Zod-схемы, если нужно
-    return {};
+  
+    return fieldDef;
   }
 
   // Функция для обработки всей Zod-схемы
-  function processZodSchema(zodObject: any): any {
-    const result: any = {};
+  function processZodSchema(zodObject: z.ZodObject<any>): any {
+    const result: Record<string, any> = {};
     for (const key in zodObject.shape) {
-      if (zodObject.shape.hasOwnProperty(key)) {
-        result[key] = processZodField(zodObject.shape[key]);
-      }
+      result[key] = processZodField(zodObject.shape[key]);
     }
     return result;
   }
