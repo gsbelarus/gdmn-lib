316a12fe8d6834ead3b8d4dcbd268ec409bd6dbe
hirurg-lybitel <yurashoihet@gmail.com>
2025-04-10 20:15:54 +0300

    revert

--- packages/gdmn-er-mongo/src/models/entity-def.ts
+++ packages/gdmn-er-mongo/src/models/entity-def.ts
@@ -2,11 +2,12 @@ import { entityDef } from 'gdmn-er';
 import { TEntityDefWithId, ZodEntityDef } from '../types/entity-def';
 import mongoose, { Model } from 'mongoose';
 import { registerModel } from '../registry';
-import { createSchema } from "@mirite/zod-to-mongoose";
+// import { createSchema } from "@mirite/zod-to-mongoose";
+import { entity2schema } from '../er2mongo';
 
-// const entityDefSchema = coreEntity2Schema<TEntityDefWithId>(entityDef);
+const entityDefSchema = entity2schema<TEntityDefWithId>(entityDef);
 
-const entityDefSchema = createSchema(ZodEntityDef);
+// const entityDefSchema = createSchema(ZodEntityDef);
 
 export const EntityDef = registerModel<TEntityDefWithId>(
   mongoose.models[entityDef.name] || mongoose.model(entityDef.name, entityDefSchema)
