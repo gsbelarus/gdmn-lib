3001244437213c38ae86c49b615d7ff9e4f98850
sunnycreature <sunnycreature@gmail.com>
2025-05-07 12:41:20 +0300

    chore: add methods in schema

--- packages/gdmn-er-mongo/src/er2mongo.ts
+++ packages/gdmn-er-mongo/src/er2mongo.ts
@@ -62,16 +62,56 @@ function mapAttrType2MongoType(attrType: AttrType): any {
   }
 }
 
-export function entity2schema<T>(
-  entity: Entity,
-  options?: Options,
-): mongoose.Schema<T> {
+function methodSchema() {
+  return new mongoose.Schema({
+    name: { type: String, required: true },
+    namespace: { type: String, required: true },
+    environment: { type: String, enum: ['server', 'client', 'both'], required: true },
+    description: { type: String },
+    params: [{
+      name: { type: String, required: true },
+      type: { type: String, required: true },
+      required: { type: Boolean, default: false },
+      nullable: { type: Boolean, default: false },
+      default: mongoose.Schema.Types.Mixed,
+    }],
+    returnType: { type: String },
+    returnDescription: { type: String },
+    code: {
+      lang: { type: String },
+      code: { type: String },
+    },
+    fn: { type: mongoose.Schema.Types.Mixed },
+    order: { type: Number, required: true },
+    disabled: { type: Boolean, default: false },
+  });
+}
+
+export function entity2schema<T>(entity: Entity, options?: Options): mongoose.Schema<T> {
   const attributes = Object.entries(entity.attributes);
+
   const schema = Object.fromEntries(
     attributes.map(([attrName, attrType]) => [
       attrName,
       mapAttrType2MongoType(attrType),
-    ]),
+    ])
   );
+
+  if (entity.parent) {
+    schema['parent'] = { type: mongoose.Schema.Types.ObjectId, ref: entity.parent.name };
+  }
+
+  if (entity.options) {
+    schema['options'] = { type: Map, of: Boolean, default: entity.options };
+  }
+
+  if (entity.methods) {
+    schema['methods'] = {
+      type: Map,
+      of: methodSchema(),
+      default: entity.methods,
+    };
+  }
+
   return new mongoose.Schema<T>(schema as any, options);
 }
