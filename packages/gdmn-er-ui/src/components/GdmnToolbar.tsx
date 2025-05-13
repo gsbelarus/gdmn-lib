f91a7706a8df60208a5f9ef176f07ad45d071e19
sashazernin <94962537+sashazernin@users.noreply.github.com>
2025-05-13 20:37:54 +0300

    feat:  GdmnToolbar theme

--- packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
+++ packages/gdmn-er-ui/src/components/GdmnToolbar.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import Tooltip from '@mui/material/Tooltip/Tooltip';
 import React, { useEffect, useState } from 'react';
 
@@ -26,12 +27,59 @@ export type GdmnToolbarItem =
 
 export type GdmnToolbarItems = GdmnToolbarItem[];
 
+export interface GdmnToolbarThemeOptions {
+  border?: string,
+  color?: string;
+  background?: string;
+}
+
+export interface GdmnToolbarThemeProps extends GdmnToolbarThemeOptions {
+  hover?: GdmnToolbarThemeOptions,
+  disabled?: GdmnToolbarThemeOptions;
+  toggled?: GdmnToolbarThemeOptions & {
+    hover?: GdmnToolbarThemeOptions,
+    disabled?: GdmnToolbarThemeOptions;
+  };
+}
+
 type GdmnToolbarProps = {
   items: GdmnToolbarItems;
   showLabels?: boolean;
+  theme?: GdmnToolbarThemeProps;
 };
 
-export function GdmnToolbar({ items, showLabels }: GdmnToolbarProps) {
+export function GdmnToolbar({ items, showLabels, theme: propsTheme }: Readonly<GdmnToolbarProps>) {
+
+  const theme: GdmnToolbarThemeProps = {
+    border: propsTheme?.border ?? 'rgb(82 82 91)',
+    color: propsTheme?.color ?? 'rgb(82 82 91)',
+    hover: {
+      border: propsTheme?.hover?.border ?? 'rgb(82 82 91)',
+      color: propsTheme?.hover?.color ?? 'white',
+      background: propsTheme?.hover?.background ?? 'rgb(0 0 0 / 10%)'
+    },
+    disabled: {
+      border: propsTheme?.disabled?.border ?? 'rgb(82 82 91 / 40%)',
+      color: propsTheme?.disabled?.color ?? 'rgb(82 82 91 / 40%)',
+      background: propsTheme?.disabled?.background ?? 'none'
+    },
+    toggled: {
+      border: propsTheme?.toggled?.border ?? 'rgb(82 82 91)',
+      color: propsTheme?.toggled?.color ?? 'white',
+      background: propsTheme?.toggled?.background ?? 'rgb(82 82 91)',
+      hover: {
+        border: propsTheme?.toggled?.hover?.border ?? 'rgb(136 136 143)',
+        color: propsTheme?.toggled?.hover?.color ?? 'white',
+        background: propsTheme?.toggled?.hover?.background ?? 'rgb(136 136 143)',
+      },
+      disabled: {
+        border: propsTheme?.toggled?.disabled?.border ?? 'rgb(63 63 70)',
+        color: propsTheme?.toggled?.disabled?.color ?? 'white',
+        background: propsTheme?.toggled?.disabled?.background ?? 'rgb(63 63 70)'
+      }
+    }
+  };
+
   const [pressed, setPressed] = useState<string | number | undefined>();
   const [animated, setAnimated] = useState<string | number | undefined>();
   const [internalLoading, setInternalLoading] = useState<string | number | undefined>();
@@ -72,27 +120,44 @@ export function GdmnToolbar({ items, showLabels }: GdmnToolbarProps) {
             </div>
           ) : undefined;
 
-        let st =
-          item.type === "separator"
-            ? ""
-            : item.toggled
-              ? item.disabled
-                ? "bg-zinc-700 fill-zinc-50 text-zinc-50"
-                : "bg-primary fill-white text-white hover:bg-primary/80 hover:fill-white hover:text-white"
-              : item.disabled
-                ? "fill-zinc-600 text-zinc-600 opacity-40"
-                : "fill-zinc-600 text-zinc-600 hover:bg-primaryHover/20 hover:fill-primary hover:text-primary";
+        const getStyles = (toggled: boolean, disabled: boolean) => {
+          const stylesFromObject = (options: GdmnToolbarThemeOptions | undefined) => {
+            return {
+              borderColor: options?.border,
+              fill: options?.color,
+              color: options?.color,
+              background: options?.background,
+            };
+          }
 
-        if (index === pressed) {
-          st += " relative top-[1px] left-[1px]";
-        } else {
-          st += " shadow";
-        }
+          if (toggled) {
+            if (disabled) {
+              return stylesFromObject(theme.toggled?.disabled)
+            }
+            return {
+              ...stylesFromObject(theme.toggled),
+              '&:hover': {
+                ...stylesFromObject(theme.toggled?.hover)
+              }
+            };
+          }
+          if (disabled) {
+            return stylesFromObject(theme.disabled)
+          }
+          return {
+            ...stylesFromObject(theme),
+            '&:hover': {
+              ...stylesFromObject(theme.hover)
+            }
+          };
+        };
 
         return B && item.type === "button" ? (
-          <div
+          <Box
             key={index}
-            className={`w-8 h-8 flex justify-center items-center border border-solid border-zinc-600 rounded ${st}`}
+            sx={getStyles(!!item.toggled, !!item.disabled)}
+            className={`w-8 h-8 flex justify-center items-center border
+              border-solid rounded ${index === pressed ? 'relative top-[1px] left-[1px]' : 'shadow'}`}
             onClick={
               item.disabled || item.loading || !item.onClick
                 ? undefined
@@ -118,7 +183,7 @@ export function GdmnToolbar({ items, showLabels }: GdmnToolbarProps) {
             ) : (
               B
             )}
-          </div>
+          </Box>
         ) : (
           <div
             key={index}
