81b2ab11e08cba54fc141059a4d50c1336c3e320
sunnycreature <sunnycreature@gmail.com>
2025-08-12 14:45:55 +0300

    chore: bump version to 0.14.105 across all packages and refactor createBaseDateWithTime parameters for improved clarity

--- packages/gdmn-utils/src/utils.ts
+++ packages/gdmn-utils/src/utils.ts
@@ -121,7 +121,7 @@ export const isValidISODate = (str: string): boolean => {
   * @param second
   * @returns {Date} A Date object representing the UTC date with the specified time.
  */
-export const createBaseDateWithTime = (isUTC: boolean, hour: number, minute: number, second: number = 0): Date => {
+export const createBaseDateWithTime = (hour: number, minute: number, second: number = 0, isUTC: boolean): Date => {
   const baseDate = new Date(0); // 1970-01-01T00:00:00.000Z
   if (isUTC) {
     baseDate.setUTCHours(hour, minute, second, 0);
